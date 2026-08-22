cd70c11cb2e2a2a7cecd2565dd71d7564307e5a5
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-08-22 00:32:25 -0400

    feat: implement a basic simple tokenizer

--- mini_llm/pretraining/tokenize.py
+++ mini_llm/pretraining/tokenize.py
@@ -19,9 +19,34 @@ print(vocab_size)
 # print the vocab 
 
 vocab = {token: integer for integer, token in enumerate(all_words)}
-#print the first 50 items of the vocabulary 
-for k,v in enumerate(vocab.items()):
-    print(v)
-    if k >= 50:
-        break 
+# #print the first 50 items of the vocabulary 
+# for k,v in enumerate(vocab.items()):
+#     print(v)
+#     if k >= 50:
+#         break 
       
+class SimpleTokenizer:
+    def __init__(self,vocab):
+        # store the vocabulary as a class attribute for accessing in the encode and decode methods
+        self.str_to_int = vocab 
+        # create an inverse vocabulary that will map token IDS back to the original text tokens 
+        self.int_to_str = {i:s for s,i in vocab.items()}
+        
+    def encode(self,text):
+        # process input text into token IDS
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)',text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids 
+    
+    def decode(self,ids):
+        # convert token IDS back to text 
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # remove spaces before the specified punctuation 
+        text = re.sub(r'\s+([,.?!"()\'])',r'\1',text) 
+        return text 
+    
+
+        
