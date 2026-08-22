b508f68b6d97b0be9ba4dfbed73475e91d87df1f
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-08-22 00:15:02 -0400

    tokenization cleanup with removing whitespace

--- mini_llm/pretraining/tokenize.py
+++ mini_llm/pretraining/tokenize.py
@@ -6,4 +6,6 @@ FILE_PATH = PROJECT_ROOT / "data" / "the_verdict.txt"
 raw_text = FILE_PATH.read_text(encoding="utf-8")
 
 result = re.split(r'([,.]|\s)',raw_text)
+# remove whitespace as well
+result = [item for item in result if item.strip()]
 print(result)
