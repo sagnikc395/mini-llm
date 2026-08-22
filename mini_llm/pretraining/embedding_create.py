631a4c21d653c12f412478fdf13c701fca1e5b88
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-08-22 03:18:27 -0400

    reached the input embeddings stage

--- mini_llm/pretraining/embedding_create.py
+++ mini_llm/pretraining/embedding_create.py
@@ -46,3 +46,9 @@ pos_embedding_layer = torch.nn.Embedding(context_length,output_dim)
 pos_embeddings = pos_embedding_layer(torch.arange(context_length))
 
 print(pos_embeddings.shape)
+
+input_embeddings = token_embeddings + pos_embeddings
+
+print(input_embeddings.shape)
+
+
