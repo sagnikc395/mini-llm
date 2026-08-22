a2318f36e9b250d56deeb96e8c58c4d52dff2897
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-08-22 00:20:07 -0400

    tokenize the entire data

--- mini_llm/pretraining/tokenize.py
+++ mini_llm/pretraining/tokenize.py
@@ -5,7 +5,9 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 FILE_PATH = PROJECT_ROOT / "data" / "the_verdict.txt"
 raw_text = FILE_PATH.read_text(encoding="utf-8")
 
-result = re.split(r'([,.]|\s)',raw_text)
-# remove whitespace as well
-result = [item for item in result if item.strip()]
-print(result)
+# preprocess the data
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+# remove whitespace as well, handles other types of punctuation, like question marks, quotation marks.
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+
