e2f00014aab89380fab8527be1431df276dfdbba
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-09-07 14:59:38 -0400

    changes

--- mini_llm/training/mini_llm_gpt.py
+++ mini_llm/training/mini_llm_gpt.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from ..config import GPT_CONFIG_124M as cfg
+from mini_llm.config import GPT_CONFIG_124M as cfg
 
 
 class GPTModel(nn.Module):
@@ -73,5 +73,40 @@ class LayerNorm(nn.Module):
         return out
 
 
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return (
+            0.5
+            * x
+            * (
+                1
+                + torch.tanh(
+                    torch.sqrt(torch.tensor(2.0 / torch.pi))
+                    * (x + 0.044715 * torch.pow(x, 3))
+                )
+            )
+        )
+
+
 if __name__ == "__main__":
-    LayerNorm().simple_layer_norm_example()
+    LayerNorm(emb_dim=6).simple_layer_norm_example()
+    # plot gelu vs relu side by side
+    import matplotlib.pyplot as plt
+
+    gelu, relu = GELU(), nn.ReLU()
+
+    x = torch.linspace(-3, 3, 100)
+    y_gelu, y_relu = gelu(x), relu(x)
+    plt.figure(figsize=(8, 3))
+    for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+        plt.subplot(1, 2, i)
+        plt.plot(x, y)
+        plt.title(f"{label} activation function")
+        plt.xlabel("x")
+        plt.ylabel(f"{label} (x)")
+        plt.grid(True)
+    plt.tight_layout()
+    plt.show()
